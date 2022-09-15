b5524e34c834c31a0713df76f847951d27a045a6
sujon-chambugong <sujonchambugong@gmail.com>
2022-09-15 15:56:01 +0600

    about page design changing

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,31 +3,52 @@ import React from "react";
 const About = () => {
   return (
     <div className="container mb-5">
-          <h1 className="mt-5 mb-5">About</h1>
       <div className="row">
-        <div className="col-12 col-md-6 col-lg-6">
-          <p>
-            Furniture is considered to be the game-changer in leading furniture
-            industry. Carefully-chosen raw material, environment-friendly business
-            practice and customer-centric approach is what made furniture a
-            beloved brand at home and abroad.
-          </p>
-          <div>
-            <h3>Our Vission</h3>
-            <h5>Design with intention</h5>
-            <p>
-             There's just one way for us to create and that is with intention. Behind the design of our pieces there's an extensive process in which we combine what inspires us with what is needed, and look for a way to make it unique and functional. We don't want to just add a new product to our collection, the idea is that each piece would transmit our values, beliefs, and the love and care that's behind them improving any environment they're introduced to This is what gives value to what we do and pushes us to continue to improve daily.
-            </p>
+          <div className="col-md-12 text-center mb-5">
+            <h1 className="mt-5">About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
           </div>
-          <div>
-            <h3>Our Mission</h3>
-            <h5>Transform how the world interacts with furniture.</h5>
-            <p>
-            We are on a mission to transform how the world interacts with furniture and home accessories through the design and crafting of pieces that are functional, unique and that have an emotional, ethic, and real value.
-            </p>
+      
+          <div className="col-md-12">
+            <h3>
+                Furniture is considered to be the game-changer in leading furniture
+                industry. Carefully-chosen raw material, environment-friendly business
+                practice and customer-centric approach is what made furniture a
+                beloved brand at home and abroad.
+              </h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 col-md-6 col-lg-6">
+              <h3>Our Vission</h3>
+              <h5>Design with intention</h5>
+              <p>
+              There's just one way for us to create and that is with intention. Behind the design of our pieces there's an extensive process in which we combine what inspires us with what is needed, and look for a way to make it unique and functional. We don't want to just add a new product to our collection, the idea is that each piece would transmit our values, beliefs, and the love and care that's behind them improving any environment they're introduced to This is what gives value to what we do and pushes us to continue to improve daily.
+              </p>
+            </div>
+          <div className="col-12 col-md-6 col-lg-6">
+
           </div>
         </div>
 
+          <div className="row">
+
+            <div className="col-12 col-md-6 col-lg-6">
+              <div>
+                <h3>Our Mission</h3>
+                <h5>Transform how the world interacts with furniture.</h5>
+                <p>
+                We are on a mission to transform how the world interacts with furniture and home accessories through the design and crafting of pieces that are functional, unique and that have an emotional, ethic, and real value.
+                </p>
+              </div>
+            </div>
+          <div className="col-12 col-md-6 col-lg-6">
+
+          </div>
+          
+        </div>
+      
+        <div className="row">
         <div className="col-12 col-md-6 col-lg-6">
           <h3>An Overview of the Furniture Industry in the world</h3>
           <p>
@@ -45,6 +66,14 @@ const About = () => {
             friendly raw materials that cause minimal waste. Lean manufacturing is
             done here to produce diverse products with minimal waste of materials,
             resources, processing, and human involvement.
+          </p>
+        </div>
+        <div className="col-12 col-md-6 col-lg-6">
+          <h3>An Overview of the Furniture Industry in the world</h3>
+          <p>
+            The domestic furniture market is worth more than BDT 67 Billion (EU
+            Technical Report). With this increasing demand for furniture in cities
+           
           </p>
         </div>
       </div>
