a60ef58cbc63ad247b3e2b5f9a96d641d0818326
sujon-chambugong <sujonchambugong@gmail.com>
2022-11-06 22:05:08 +0600

    added render link

--- src/components/Product/HomeFurniture.js
+++ src/components/Product/HomeFurniture.js
@@ -6,7 +6,7 @@ const HomeFurniture = () => {
     const navigate = useNavigate();
 
     useEffect( ()=>{
-        fetch('https://sleepy-thicket-05560.herokuapp.com/furniture')
+        fetch('https://furniture-furnished-server.onrender.com/furniture')
         .then(res => res.json())
         .then(data => setFurnitures(data));
     }, []);
