7f0e9e53fa6e6761bfcf24c1367f935696112e04
sujon-chambugong <sujonchambugong@gmail.com>
2022-11-06 22:07:00 +0600

    link updated

--- src/hooks/useReview.js
+++ src/hooks/useReview.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 const useReview = () => {
     const [reviews, setReviews] = useState([]);
     useEffect( () =>{
-        fetch('https://sleepy-thicket-05560.herokuapp.com/reviews')
+        fetch('https://furniture-furnished-server.onrender.com/reviews')
         .then(res => res.json())
         .then(data => setReviews(data));
     }, []);
